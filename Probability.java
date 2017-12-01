28212fea84186e5c4644393f26174a8af07ea985
annamstarr <31353295+annamstarr@users.noreply.github.com>
2017-12-01 09:00:07 -0800

    ok

---
+++ Probability.java
@@ -0,0 +1,30 @@
+
+public class Probability implements ProbabilityCalc, Comparable<Probability> {
+    // instance variables - replace the example below with your own
+    private double prob;
+    private int favorable;
+    private int sampleSpace;
+    
+    public Probability(int fav, int sample) {
+        this.favorable = fav;
+        this.sampleSpace = sample;
+        prob = ((double)favorable)/sampleSpace;
+    }
+    public double probabilityAND(Probability p2) {
+         return this.prob * p2.prob;
+    }
+    public double probabilityOR(Probability p2, double overlap) {
+        return this.prob + p2.prob - overlap;
+    }
+    public int compareTo(Probability other) {
+        if(this.prob > other.prob) {
+            return 1;
+        }
+        else if(this.prob < other.prob) {
+            return -1;
+        }
+        else {
+            return 0;
+        }
+    }
+}
---
+++ ProbabilityDriver.java
@@ -0,0 +1,10 @@
+public class ProbabilityDriver {
+    public static void main (String[] args) {
+        Probability p1 = new Probability(6,10);
+        Probability p2 = new Probability(5,10);
+        p1.probabilityAND(p2);
+        p1.compareTo(p2);
+        
+    }
+    
+}
